4435239809abe53f668926beb52107320e301d82
geiselrocha <geiselrocha@hotmail.com>
2022-06-24 00:27:16 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,28 @@
-test(' ', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { About } from '../pages';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente About.js', () => {
+  test('Se a página contém as informações sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+  });
+
+  test('Se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const aboutPokedex = screen.getByRole('heading', { name: /About Pokédex/i });
+    expect(aboutPokedex).toBeInTheDocument();
+  });
+
+  test('Se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const twoParagraphs = screen.getAllByText(/pokémons/i);
+    expect(twoParagraphs).toHaveLength(2);
+  });
+
+  test('Se a página contém a seguinte imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+    const pokédexImage = screen.getByRole('img', { name: /pokédex/i });
+    expect(pokédexImage).toHaveProperty('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
