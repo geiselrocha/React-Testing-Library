9774bcd44f3d36c6e377d921a5519970558b0fb0
geiselrocha <geiselrocha@hotmail.com>
2022-06-24 01:04:53 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { MemoryRouter, Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import App from '../App';
+
+describe('Teste o componente FavoritePokemons.js', () => {
+  test('Se é exibida na tela a mensagem No favorite pokemon found', () => {
+    render(<MemoryRouter><FavoritePokemons /></MemoryRouter>);
+    const noFavPokeFound = screen.getByText(/No favorite pokemon found/i);
+    expect(noFavPokeFound).toBeInTheDocument();
+  });
+
+  test('Se são exibidos todos os cards de pokémons favoritados', () => {
+    render(<Router history={ createMemoryHistory() }><App /></Router>);
+    const favoritePokémon = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(favoritePokémon);
+  });
+});
