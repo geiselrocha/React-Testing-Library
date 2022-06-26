9def9fdc13049626875eca60d74969634ddedeca
geiselrocha <geiselrocha@hotmail.com>
2022-06-26 01:16:08 -0300

    Requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,50 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente Pokemon.js', () => {
+  test('Se é renderizado um card com as informações de determinado pokémon', () => {
+    renderWithRouter(<App />);
+    const pokemonName = screen.getByTestId('pokemon-name');
+    const pokemonType = screen.getByTestId('pokemon-type');
+    const pokemonWeight = screen.getByTestId('pokemon-weight');
+    const pokemonImage = screen.getByRole('img', { name: /pikachu sprite/i });
+    const URL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png';
+    expect(pokemonName).toBeInTheDocument('Pikachu');
+    expect(pokemonType).toHaveTextContent('Electric');
+    expect(pokemonWeight).toBeInTheDocument();
+    expect(pokemonImage.src).toContain(URL, 'alt');
+  });
+
+  test('Se o card do pokémon indicado na Pokédex contém um link de navegação', () => {
+    renderWithRouter(<App />);
+    const moreDetails = screen.getByRole('link', { name: /more details/i });
+    expect(moreDetails).toBeInTheDocument();
+  });
+
+  test('Se ao clicar no link de navegação do pokémon, é feito o redirecionamento', () => {
+    renderWithRouter(<App />);
+    const moreDetails = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(moreDetails);
+    expect(moreDetails).toHaveAttribute('href', '/pokemons/25');
+  });
+
+  test('Se a URL exibida no navegador muda para pokemon/<id>', () => {
+    renderWithRouter(<App />);
+    const moreDetails = screen.getByRole('link', { name: /more details/i });
+    expect(moreDetails.href).toContain('/pokemons/25');
+  });
+
+  test('Se existe um ícone de estrela nos pokémons favoritados', () => {
+    renderWithRouter(<App />);
+    const moreDetails = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(moreDetails);
+    const checkbox = screen.getByRole('checkbox');
+    userEvent.click(checkbox);
+    const starIcon = screen.getByRole('img', { name: /pikachu is marked as favorite/i });
+    expect(starIcon.src).toContain('/star-icon.svg');
+    expect(starIcon).toHaveProperty('alt', 'Pikachu is marked as favorite');
+  });
+});
